3e2bf3dd3393bf03856a4a8277c69ec068be5502
Muhammad Raafay <raafayvohra.mrv@gmail.com>
2025-03-18 15:43:45 +0100

    add test to verify counter functionality

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -31,3 +31,31 @@ test.describe("Authentication Tests", () => {
   });
 });
 
+//Test 2
+test("Counter should increment, decrement, and reset correctly", async ({
+    page,
+  }) => {
+    await page.goto("/login");
+  
+    await page.locator("#email-input").fill("test@maddox123.ai");
+    await page.locator("#password-input").fill("supersecure");
+    await page.getByRole("button", { name: "Login" }).click();
+  
+    await expect(page).toHaveURL("/");
+    await expect(page.getByRole("paragraph")).toHaveText("0");
+  
+    await page.getByRole("button", { name: "+" }).click();
+    await expect(page.getByRole("paragraph")).toHaveText("1");
+  
+    await page.getByRole("button", { name: "-" }).click();
+    await expect(page.getByRole("paragraph")).toHaveText("0");
+  
+    await page.getByRole("button", { name: "+" }).click();
+    await page.getByRole("button", { name: "+" }).click();
+    await expect(page.getByRole("paragraph")).toHaveText("2");
+  
+    await page.getByRole("button", { name: "Reset" }).click();
+    await expect(page.getByRole("paragraph")).toHaveText("0");
+  });
+  
+ 
